925f1bb12b7d7c1d84f9c15e15097228da5e0bec
shaqarava <14287229+shaqarava@users.noreply.github.com>
2020-05-25 01:37:13 -0400

    Disambiguate rejson method names.

--- aioredis/commands/rejson.py
+++ aioredis/commands/rejson.py
@@ -21,7 +21,7 @@ class JSONCommandsMixin:
             self.decoder = decoder
             self.decode = getattr(self.decoder, 'loads')
 
-    async def set(self, key, path, jsonable, nx=False, xx=False):
+    async def jset(self, key, path, jsonable, nx=False, xx=False):
         """
         Set the JSON value at ``key`` under the ``path`` to ``jsonable``
         ``nx`` if set to True, set ``value`` only if it does not exist
@@ -37,7 +37,7 @@ class JSONCommandsMixin:
             pieces.append('XX')
         return await self.execute(b'JSON.SET', key, path, *pieces)
 
-    async def get(self, key, *paths):
+    async def jget(self, key, *paths):
         """
         Get the object stored as a JSON value at ``key``
         ``paths`` is zero or more paths, and defaults to root path
@@ -45,13 +45,13 @@ class JSONCommandsMixin:
         result = await self.execute('JSON.GET', key, *paths)
         return self.decode(result)
 
-    async def delete(self, key, path='.'):
+    async def jdel(self, key, path='.'):
         """
         Deletes the JSON value stored at ``key`` under ``path``
         """
         return await self.execute(b'JSON.DEL', key, path)
 
-    async def mget(self, *keys, path='.'):
+    async def jmget(self, *keys, path='.'):
         """
         Gets the objects stored as a JSON values under ``path`` from
         ``keys``
@@ -59,41 +59,41 @@ class JSONCommandsMixin:
         results = await self.execute('JSON.MGET', *keys, path)
         return [self.decode(obj) for obj in results]
 
-    async def type(self, key, path='.'):
+    async def jtype(self, key, path='.'):
         """
         Gets the type of the JSON value under ``path`` from ``key``
         """
         return await self.execute(b'JSON.TYPE', key, path)
 
-    async def numincrby(self, key, path, number):
+    async def jnumincrby(self, key, path, number):
         """
         Increments the numeric (integer or floating point) JSON value under
         ``path`` at ``key`` by the provided ``number``
         """
         return await self.execute(b'JSON.NUMINCRBY', key, path, number)
 
-    async def nummultby(self, key, path, number):
+    async def jnummultby(self, key, path, number):
         """
         Multiplies the numeric (integer or floating point) JSON value under
         ``path`` at ``key`` with the provided ``number``
         """
         return await self.execute(b'JSON.NUMMULTBY', key, path, number)
 
-    async def strappend(self, key, string, path='.'):
+    async def jstrappend(self, key, string, path='.'):
         """
         Appends to the string JSON value under ``path`` at ``key`` the
         provided ``string``
         """
         return await self.execute(b'JSON.STRAPPEND', key, path, self.encode(string))
 
-    async def strlen(self, key, path='.'):
+    async def jstrlen(self, key, path='.'):
         """
         Returns the length of the string JSON value under ``path`` at
         ``key``
         """
         return await self.execute(b'JSON.STRLEN', key, path)
 
-    async def arrappend(self, key, path, *jsonables):
+    async def jarrappend(self, key, path, *jsonables):
         """
         Appends the objects ``args`` to the array under the ``path` in
         ``key``
@@ -103,7 +103,7 @@ class JSONCommandsMixin:
             pieces.append(self.encode(o))
         return await self.execute(b'JSON.ARRAPPEND', key, path, *pieces)
 
-    async def arrindex(self, key, path, scalar, start=0, stop=-1):
+    async def jarrindex(self, key, path, scalar, start=0, stop=-1):
         """
         Returns the index of ``scalar`` in the JSON array under ``path`` at
         ``key``. The search can be limited using the optional inclusive
@@ -111,7 +111,7 @@ class JSONCommandsMixin:
         """
         return self.execute(b'JSON.ARRINDEX', key, path, self.encode(scalar), start, stop)
 
-    async def arrinsert(self, key, path, index, *args):
+    async def jarrinsert(self, key, path, index, *args):
         """
         Inserts the objects ``args`` to the array at index ``index`` under the
         ``path` in ``key``
@@ -121,35 +121,35 @@ class JSONCommandsMixin:
             pieces.append(self.encode(o))
         return self.execute(b'JSON.ARRINSERT', key, path, index, *pieces)
 
-    async def arrlen(self, key, path='.'):
+    async def jarrlen(self, key, path='.'):
         """
         Returns the length of the array JSON value under ``path`` at
         ``key``
         """
         return self.execute(b'JSON.ARRLEN', key, path)
 
-    async def arrpop(self, key, path='.', index=-1):
+    async def jarrpop(self, key, path='.', index=-1):
         """
         Pops the element at ``index`` in the array JSON value under ``path`` at
         ``key``
         """
         return self.execute(b'JSON.ARRPOP', key, path, index)
 
-    async def arrtrim(self, key, path, start, stop):
+    async def jarrtrim(self, key, path, start, stop):
         """
         Trim the array JSON value under ``path`` at ``key`` to the
         inclusive range given by ``start`` and ``stop``
         """
         return self.execute(b'JSON.ARRTRIM', key, path, start, stop)
 
-    async def objkeys(self, key, path='.'):
+    async def jobjkeys(self, key, path='.'):
         """
         Returns the key names in the dictionary JSON value under ``path`` at
         ``key``
         """
         return self.execute(b'JSON.OBJKEYS', key, path)
 
-    async def objlen(self, key, path='.'):
+    async def jobjlen(self, key, path='.'):
         """
         Returns the length of the dictionary JSON value under ``path`` at
         ``key``
